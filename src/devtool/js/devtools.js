61ca62848425c8929923a7cedf4324777e2628c5
Gil Birman <gbirman@iseatz.com>
2014-12-11 00:37:09 -0600

    todos in devtools.js

--- src/devtool/js/devtools.js
+++ src/devtool/js/devtools.js
@@ -2,11 +2,16 @@
 // chrome.devtools.*
 // chrome.extension.*
 
+// @todo: use chrome.devtools.inspectedWindow.eval for
+//        - checking for presence of simflux on window
+//        - injecting scripts?
+
 // Create a tab in the devtools area
 chrome.devtools.panels.create("simflux", "simflux.png", "panel.html", function(panel) {});
 
 // @todo: we can intercept loading of simflux.js here... utilize this?
-//chrome.devtools.inspectedWindow.onResourceAdded.addListener(function() {
-//  console.log("onResourceAdded-->arguments: ", arguments[0].url);
+//.devtools.inspectedWindow.onResourceAdded.addListener(function() {
+  // possibly use chrome.runtime.sendMessage
+  //console.log("onResourceAdded-->arguments: ", arguments, arguments[0].url);
 //});
 

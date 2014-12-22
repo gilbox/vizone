707e2cfd75a1b8d664431bba6c1c85cb30662f3f
gilbox <birmangil@gmail.com>
2014-12-22 02:12:31 -0500

    vizone: prevent double-load

--- dist/vizone-bundle.js
+++ dist/vizone-bundle.js
@@ -624,6 +624,9 @@ Zone.init();
 })();
 }
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+// prevent double-load
+if (window.vizone) return;
+
 window.vizone = require('./vizone');
 console.log("%cvizone loaded", "color:white; background-color:orange; font-size: 14pt; border-radius:8px; padding: 0 10px; font-family:Verdana;");
 
--- src/vizone/index.js
+++ src/vizone/index.js
@@ -1,2 +1,5 @@
+// prevent double-load
+if (window.vizone) return;
+
 window.vizone = require('./vizone');
 console.log("%cvizone loaded", "color:white; background-color:orange; font-size: 14pt; border-radius:8px; padding: 0 10px; font-family:Verdana;");

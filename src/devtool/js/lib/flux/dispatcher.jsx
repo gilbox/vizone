b8e4870acd3bcfa414ec1a8a791db4f4db00de5a
gilbox <birmangil@gmail.com>
2014-12-24 04:53:51 -0600

    switch to constructor syntax for simflux.Dispatcher

--- src/devtool/js/lib/flux/dispatcher.jsx
+++ src/devtool/js/lib/flux/dispatcher.jsx
@@ -1,5 +1,5 @@
 var simflux = require('simflux');
 
-var dispatcher = simflux.instantiateDispatcher('simflux-viz dispatcher');
+var dispatcher = new simflux.Dispatcher('simflux-viz dispatcher');
 
 module.exports = dispatcher;
